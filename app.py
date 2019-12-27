f8da5394afc3ac9d76e88bc65703d0ec291ae87d
Edward Li <edwardyaoli@gmail.com>
2019-12-26 23:01:34 -0500

    fixed host and port location

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import gensim
-app = Flask(__name__, host='0.0.0.0', port=5001, static_url_path='', static_folder='static')
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route("/get_vec/<string:word>/")
 def getWord(word):
@@ -11,4 +11,4 @@ def getWord(word):
 
 if __name__ == '__main__':
     model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
-    app.run()
+    app.run(host='0.0.0.0', port=5001)
