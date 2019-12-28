ae618e4bbe5a1eb3c30b1c0112bd5e6af9a0360a
Edward Li <edwardyaoli@gmail.com>
2019-12-27 23:10:08 -0500

    added axis definitions and generation

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ import numpy as np
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
-AXES = {
-    'test1': np.ones(300),
-    'test2': np.random.rand(300)
+AXES = {}
+
+AXIS_DEFS = {
+    'gender': [('man', 'woman'), ('men', 'women'), ('he', 'she'), ('him', 'her'), ('his', 'her'), ('his', 'hers'), ('boy', 'girl'), ('boys', 'girls'), ('male', 'female'), ('masculine', 'feminine')],
+    'class': [('rich', 'poor'), ('richer', 'poorer'), ('richest', 'poorest'), ('affluence', 'poverty'), ('affluent', 'impoverished'), ('expensive', 'inexpensive'), ('luxury', 'cheap'), ('opulent', 'needy')],
+    'race': [('black', 'white'), ('blacks', 'whites'), ('Blacks', 'Whites'), ('Black', 'White'), ('African', 'European'), ('African', 'Caucasian')]
 }
 
 @app.route("/get_vec/<string:word>/")
@@ -72,14 +75,28 @@ class DebugModel():
         return self.rand_dict
 
 if __name__ == '__main__':
+    # Check for debug mode
     ap = argparse.ArgumentParser()
     ap.add_argument('--debug', action='store_true',
                     help="use random data instead of loading model")
     args = vars(ap.parse_args())
 
+    # Selectively load model based on debug mode
+    print("[INFO] Loading model...")
     if args['debug']:
+        print("DEBUG MODE")
         model = DebugModel(300)
     else:
         model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
+    
+    # Generate axes
+    print("[INFO] Generating axes")
+    for axis in AXIS_DEFS:
+        word_pairs = AXIS_DEFS[axis]
+        all_axis_vecs = [model.wv[wp[0]] - model.wv[wp[1]] for wp in word_pairs]
+        AXES[axis] = np.average(all_axis_vecs, axis=0)
+        
+    print("[INFO] Available axes:")
+    print(list(AXES.keys())
 
     app.run(host='0.0.0.0', port=5001)
